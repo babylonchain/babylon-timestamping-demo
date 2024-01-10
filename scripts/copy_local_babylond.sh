1ba62a798afc609eba2e273c7b96910b88e1becc
Mauro Lacy <mauro@lacy.com.es>
2024-01-10 14:01:59 +0100

    Add Mac M1 (arm64) binary and local copy script

---
+++ scripts/copy_local_babylond.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck >/dev/null && shellcheck "$0"
+
+BIN="babylond"
+SOURCE_REPO="babylon-private"
+SOURCE_FOLDER="$(dirname "$0")/../../$SOURCE_REPO"
+OUTPUT_FOLDER="$(dirname "$0")/../$SOURCE_REPO"
+
+echo "DEV-only: copy from locally built"
+cp -f  "$SOURCE_FOLDER"/build/${BIN} "$OUTPUT_FOLDER/$BIN-$(uname -m)"
+
+cd "$SOURCE_FOLDER"
+TAG=$(git rev-parse HEAD)
+cd - 2>/dev/null
+echo "$TAG" >"$OUTPUT_FOLDER/version.txt"
